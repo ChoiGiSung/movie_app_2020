8cea11c30d4202e5e1564454ee559a3688c02341
ChoiGiSung <chlrltjd159@naver.com>
2020-08-10 22:42:56 +0900

    proptype과 푸드예제

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import PropTypes from "prop-types";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App">
+
+          {data.map(ss => (
+
+              < Face key={ss.id} name={ss.name} age={ss.age} sex={ss.sex} rating={ss.rating} />
+              ))}
+         
     </div>
   );
 }
 
-export default App;
+
+
+
+
+
+const data = [
+    {
+        id:"1",
+        name: "d",
+        age: "1",
+        sex: "2",
+        rating:5
+    },
+    {
+        id:"2",
+        name: "sd",
+        age: "d",
+        sex: "d",
+        rating: 4
+    },
+    {
+        id:"3",
+        name: "a",
+        age: "d",
+        sex: "b",
+        rating: 3
+    },
+    {
+        id:"4",
+        name: "q",
+        age: "r",
+        sex: "x",
+        rating: 2
+    }
+];
+
+function Face(gu) {
+   
+    return (
+       
+        <div>
+            <h1>{gu.name}d</h1>
+            <h2>{gu.age}</h2>
+            <h2>{gu.sex}</h2>
+            <h2>{gu.rating}/5</h2>
+        </div>);
+}
+
+Face.propTypes = { //값 이 잘 들어갔는지 확인 잘안들어가면 그때 오류문구 발생
+    name: PropTypes.string.isRequired,
+    age: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired
+};
+
+
+        export default App;

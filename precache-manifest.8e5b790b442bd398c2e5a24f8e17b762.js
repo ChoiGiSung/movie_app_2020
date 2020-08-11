11213fc4968b7c28d6e68ea58e3e3c6c256d0820
ChoiGiSung <chlrltjd159@naver.com>
2020-08-12 01:41:46 +0900

    Updates

---
+++ precache-manifest.8e5b790b442bd398c2e5a24f8e17b762.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64849bbf3d0df9dcbb4f6318102aab62",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "c311725b81390a3bf744",
+    "url": "/movie_app_2020/static/css/main.900079f3.chunk.css"
+  },
+  {
+    "revision": "3dd18e87ab088f0503d7",
+    "url": "/movie_app_2020/static/js/2.56ed097a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.56ed097a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c311725b81390a3bf744",
+    "url": "/movie_app_2020/static/js/main.8a363499.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
